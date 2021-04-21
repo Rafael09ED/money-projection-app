27e244a98687b73fedc409628a5494887ed22021
Rafael09ED <starmarstrek@gmail.com>
2021-04-21 00:05:42 -0400

    calcualteMoneySteps tested

--- src/BalanceSimulator.test.ts
+++ src/BalanceSimulator.test.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import { } from '@testing-library/react';
-import { shouldModifierBeCounted, MoneyModifier } from './BalanceSimulator';
+import { DateTime, Duration } from "luxon";
+import _ from 'lodash';
+import { shouldModifierBeCounted, MoneyModifier, calcualteMoneySteps } from './BalanceSimulator';
 
 test('shouldModifierBeCounted', () => {
   expect(shouldModifierBeCounted(<MoneyModifier><any>{ tags: [] }, [])).toBe(true);
@@ -10,4 +12,25 @@ test('shouldModifierBeCounted', () => {
   expect(shouldModifierBeCounted(<MoneyModifier><any>{ tags: ['a'] }, ['a', 'b'])).toBe(true);
   expect(shouldModifierBeCounted(<MoneyModifier><any>{ tags: ['a', 'c'] }, ['a', 'b'])).toBe(true);
   expect(shouldModifierBeCounted(<MoneyModifier><any>{ tags: ['c'] }, ['a', 'b'])).toBe(false);
+});
+
+test('calcualteMoneySteps', () => {
+  expect(
+    calcualteMoneySteps(
+      1,
+      DateTime.fromISO("2017-05-15"),
+      DateTime.fromISO("2017-05-15"),
+      Duration.fromISO('P1D'),
+      [DateTime.fromISO("2017-05-14"), DateTime.fromISO("2017-05-15"), DateTime.fromISO("2017-05-16"), DateTime.fromISO("2017-05-17")]
+    )
+  ).toStrictEqual([0, 1, 1]);
+  expect(
+    calcualteMoneySteps(
+      1,
+      DateTime.fromISO("2017-05-15"),
+      DateTime.fromISO("2017-05-20"),
+      Duration.fromISO('P1D'),
+      [DateTime.fromISO("2017-05-14"), DateTime.fromISO("2017-05-15"), DateTime.fromISO("2017-05-16"), DateTime.fromISO("2017-05-17")]
+    )
+  ).toStrictEqual([0, 1, 2]);
 });
--- src/BalanceSimulator.ts
+++ src/BalanceSimulator.ts
@@ -1,13 +1,19 @@
-import moment from 'moment'
+import { DateTime, Duration } from "luxon";
+import _ from 'lodash';
 
-type DeltaTime = any;
-type Date = any;
+type Date = DateTime;
 type Tag = string;
+
+type DeltaTime = Duration;
+// interface DeltaTime {
+//     unit: string,
+//     amount: number
+// };
 
 interface MoneyModifier {
     title: string,
     cost: number,
-    frequency: DeltaTime,
+    frequency: DeltaTime, // Could convert to function
     tags: Tag[],
     startDate: Date,
     endDate: Date
@@ -20,7 +26,7 @@ interface TableOptions {
     tags: Tag[][]
 }
 
-const shouldModifierBeCounted = (modifier: MoneyModifier, tagsToInclude: string[]): boolean => {
+const modifierShouldBeCounted = (modifier: MoneyModifier, tagsToInclude: string[]): boolean => {
     if (modifier.tags.length <= 0) return true;
     for (let i of tagsToInclude)
         for (let j of modifier.tags)
@@ -29,16 +35,74 @@ const shouldModifierBeCounted = (modifier: MoneyModifier, tagsToInclude: string[
     return false;
 }
 
-const moneyGeneratedDurringTime = (tickStart: Date, tickEnd: Date, modifier: MoneyModifier) => {
 
+const calcualteMoneySteps = (cost: number, startDate: Date, endDate: Date, frequency: DeltaTime, dateSteps: Date[]): number[] => {
+    if (startDate > endDate) throw "End Date Before Start Date";
+
+    const answer: number[] = new Array(dateSteps.length - 1).fill(0);
+
+    let rollingSum = 0;
+    let currentDateStep = 0;
+
+    let timePointer = startDate;
+    while (timePointer < _.first(dateSteps)!)
+        rollingSum += cost;
+
+    while (timePointer > dateSteps[currentDateStep]) {
+        currentDateStep++;
+        if (currentDateStep >= dateSteps.length)
+            return answer;
+    }
+
+    for (;
+        ((startDate == endDate && endDate == timePointer)|| timePointer <= endDate)
+        && timePointer < _.last(dateSteps)!
+        ;
+        timePointer = timePointer.plus(frequency)
+    ) {
+
+        while (dateSteps[currentDateStep] < timePointer) {
+            answer[currentDateStep] = rollingSum;
+            currentDateStep++;
+        }
+        // add cost to the right Date group
+        rollingSum += cost;
+    }
+
+    for (; currentDateStep < answer.length; currentDateStep++)
+        answer[currentDateStep] = rollingSum;
+
+
+    return answer;
 }
 
+// old headers
 //startDate: Date,    endDate: Date,    timeDelta: DeltaTime
-const convertFromModifiersToDateMoneyMap = (modifiers: MoneyModifier[], dateSteps: Date[], tagsGroups: Tag[][]): number[][] => {
-    return tagsGroups.map((filterTags :Tag[]): number[] => {
-        
-    });
+/*
+* modifiers & tagsGroups must be same length
+*/
+const getTimelineFromTagGroup = (modifiers: MoneyModifier[], dateSteps: Date[], tagsGroups: Tag[]): number[] => {
+    return modifiers
+        .reduce(
+            (accumulator, modifier): number[] => {
+                if (!modifierShouldBeCounted(modifier, tagsGroups))
+                    return accumulator;
+
+                return _.unzipWith(
+                    [
+                        accumulator,
+                        calcualteMoneySteps(modifier.cost, modifier.startDate, modifier.endDate, modifier.frequency, dateSteps)
+                    ],
+                    _.add
+                );
+            },
+            <number[]>new Array(dateSteps.length).fill(0)
+        );
+}
+
+const convertFromModifiersToDateMoneyTimeline = (modifiers: MoneyModifier[], dateSteps: Date[], tagsGroups: Tag[][]): number[][] => {
+    return tagsGroups.map((filterTags: Tag[]): number[] => getTimelineFromTagGroup(modifiers, dateSteps, filterTags));
 }
 
 export type { MoneyModifier, TableOptions }
-export { shouldModifierBeCounted, convertFromModifiersToDateMoneyMap };
+export { modifierShouldBeCounted as shouldModifierBeCounted, convertFromModifiersToDateMoneyTimeline, calcualteMoneySteps };
